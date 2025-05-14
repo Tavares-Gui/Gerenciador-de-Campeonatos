f4e4a1d076a0f637b3bf6c2cb268898114712ce5
Brian0505 <briancris2003@gmail.com>
2025-05-14 20:20:15 -0300

    Ajustando a App

--- esports-front/src/App.jsx
+++ esports-front/src/App.jsx
@@ -1,33 +1,9 @@
 import React, { useState } from 'react';
-import Login from './Components/Login/Login';
-import Register from './Components/Register/Register';
 
 const App = () => {
-  const [isLogin, setIsLogin] = useState(true);
-
-  const handleLogin = (credentials) => {
-    console.log('Login com:', credentials);
-    
-  };
-
-  const handleRegister = (credentials) => {
-    console.log('Registro com:', credentials);
-    
-  };
-
   return (
     <div className="app">
-      {isLogin ? (
-        <Login 
-          onLogin={handleLogin} 
-          onSwitchToRegister={() => setIsLogin(false)} 
-        />
-      ) : (
-        <Register 
-          onRegister={handleRegister} 
-          onSwitchToLogin={() => setIsLogin(true)} 
-        />
-      )}
+      
     </div>
   );
 };
